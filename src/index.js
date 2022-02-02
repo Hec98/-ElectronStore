7019ad4f60e22ce68cf0fc5a8ae438cac3ea04cd
Hec98 <hector982015@gmail.com>
2022-02-01 20:08:59 -0600

    Create window for new product

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 const url = require('url')
 const path = require('path')
 
 if(process.env.NODE_ENV !== 'production')
-require('electron-reload')(__dirname,{})
+  require('electron-reload')(__dirname,{
+    electron: path.join(__dirname, '../node_modules', '.bin', 'electron')
+  })
 
 let mainWindow;
+let newProductWindow;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({width: 800, height: 600})
@@ -13,6 +16,35 @@ app.on('ready', () => {
   mainWindow.loadFile(url.format(
     path.join(__dirname, 'views/index.html'), 
     { protocol: 'file', slashes: true }
-  ))
+  ));
 
+  const mainMenu = Menu.buildFromTemplate(templateMetu);
+  Menu.setApplicationMenu(mainMenu);
+  
 });
+
+const createNewProductWindow = () => {
+  newProductWindow =new BrowserWindow({
+    width: 400,
+    height: 400,
+    title: 'Add A New Product'
+  });
+
+  newProductWindow.setMenu(null);
+  
+  newProductWindow.loadFile(url.format(
+    path.join(__dirname, 'views/new-product.html'), 
+    { protocol: 'file', slashes: true }
+  ));
+}
+
+const templateMetu = [{
+  label: 'File',
+  submenu: [{
+      label: 'New Product',
+      accelerator: 'Ctrl+N',        
+      click() {
+        createNewProductWindow();
+      }
+    }]
+  }]
